431718f93224a03dc31014e39fd685b0b574eff8
TheDandyCodes <ricardoprietoaiml@gmail.com>
2024-12-07 19:14:39 +0100

    transform dataset

---
+++ Exercises/Unit_3/Association-Rules/transform_dataset.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from mlxtend.preprocessing import TransactionEncoder
+from pathlib import Path
+
+# Load the dataset
+notebook_dir = Path().resolve()
+movilens = pd.read_csv( notebook_dir / 'movilens_dataset/movies.csv')
+
+# Transform the dataset
+movilens['genres'] = movilens['genres'].str.split('|')
+te = TransactionEncoder()
+te_ary = te.fit(movilens['genres']).transform(movilens['genres'])
+movilens = pd.DataFrame(te_ary, columns=te.columns_)
+
+print(f"{movilens.head()}\n")
+
+# Support
+print(" ## Support ##")
+print(f"{movilens.mean()}\n")
