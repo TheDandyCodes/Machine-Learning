20831d1616477f410b18b8ac7598506331a61f66
TheDandyCodes <ricardoprietoaiml@gmail.com>
2024-12-08 15:20:01 +0100

    Transformed dataset and set index to movie titles

--- Exercises/Unit_3/Association-Rules/transform_dataset.py
+++ Exercises/Unit_3/Association-Rules/transform_dataset.py
@@ -10,10 +10,13 @@ movilens = pd.read_csv( notebook_dir / 'movilens_dataset/movies.csv')
 movilens['genres'] = movilens['genres'].str.split('|')
 te = TransactionEncoder()
 te_ary = te.fit(movilens['genres']).transform(movilens['genres'])
-movilens = pd.DataFrame(te_ary, columns=te.columns_)
+movilens = pd.DataFrame(te_ary, columns=te.columns_).set_index(movilens['title'])
 
 print(f"{movilens.head()}\n")
 
 # Support
 print(" ## Support ##")
 print(f"{movilens.mean()}\n")
+
+# -------------------------------------
+
