dbed60534edf21c3f98f20a4a19e6ae5f9bbd32d
TheDandyCodes <ricardoprietoaiml@gmail.com>
2024-12-14 18:10:59 +0100

    Transformed dataset and generated association rules

--- Exercises/Unit_3/Association-Rules/transform_dataset.py
+++ Exercises/Unit_3/Association-Rules/transform_dataset.py
@@ -6,6 +6,7 @@ import math
 from typing import Iterable
 from itertools import combinations
 import time
+from mlxtend.frequent_patterns import apriori, association_rules
 
 # Load the dataset
 notebook_dir = Path().resolve()
@@ -259,49 +260,104 @@ def get_rules_metrics_df(rules: list[tuple[list[str], list[str]]], onehot_datase
         rules_metrics['lift'].append(rule_metric['lift'])
     return pd.DataFrame(rules_metrics)
 
-# Número de reglas de tipo A -> B para este dataset
-n_subitemset = 2
-print(f"\nNumber of rules of type A -> B: {get_number_of_rules(n_itemset=len(movilens.columns), n_subitemset=n_subitemset)}")
+# # Número de reglas de tipo A -> B para este dataset
+# n_subitemset = 2
+# print(f"\nNumber of rules of type A -> B: {get_number_of_rules(n_itemset=len(movilens.columns), n_subitemset=n_subitemset)}")
 
-# Se ha calculado el tiempo que tarda en obtener todas las reglas posibles de tipo A -> B
-start = time.time()
-rules = get_rules(movilens.columns, n_subitemset=2)
-end = time.time()
-rules_metrics_df = get_rules_metrics_df(rules, movilens)
+# # Se ha calculado el tiempo que tarda en obtener todas las reglas posibles de tipo A -> B
+# start = time.time()
+# rules = get_rules(movilens.columns, n_subitemset=2)
+# end = time.time()
+# rules_metrics_df = get_rules_metrics_df(rules, movilens)
+# rules_metrics_df = rules_metrics_df.sort_values(by=['support', 'confidence', 'lift'], ascending=[False, False, False])
+
+# print(f"\nTime elapsed to generate the rules: {end - start} segs. ")
+
+# print(rules_metrics_df.head(20))
+
+# # Número de reglas de tipo A,B -> C ó A -> B,C para este dataset
+# n_subitemset = 3
+# print(f"\nNumber of rules of type A,B -> C or A -> B,C: {get_number_of_rules(n_itemset=len(movilens.columns), n_subitemset=n_subitemset)}")
+
+# start = time.time()
+# rules = get_rules(movilens.columns, n_subitemset=3)
+# end = time.time()
+# rules_metrics_df = get_rules_metrics_df(rules, movilens)
+# rules_metrics_df = rules_metrics_df.sort_values(by=['support', 'confidence', 'lift'], ascending=[False, False, False])
+
+# print(f"\nTime elapsed to generate the rules: {end - start} segs. ")
+# print(rules_metrics_df.head(20))
+
+# # Número de reglas de 9 elementos para este dataset. Considero que cada antecendete puede tener un máximo de 8 elementos
+# # y un mínimo de 1 elemento
+# n_subitemset = 9
+# print(f"\nNumber of rules for {n_subitemset} itemset: {get_number_of_rules(n_itemset=len(movilens.columns), n_subitemset=n_subitemset)}")
+
+# start = time.time()
+# rules = get_rules(movilens.columns, n_subitemset=9)
+# end = time.time()
+# print(f"\nTime elapsed to generate the rules: {end - start} segs. ")
+
+# # Número de reglas de todo tipo que contengan desde 1 hasta 19 elementos (todas las reglas posibles)
+# n_subitemset = len(movilens.columns)
+# print(f"\nNumber of rules for {n_subitemset} itemset: {get_number_of_rules(n_itemset=len(movilens.columns), n_subitemset=n_subitemset)}")
+
+# start = time.time()
+# rules = get_rules(movilens.columns, n_subitemset=len(movilens.columns))
+# end = time.time()
+# print(f"\nTime elapsed to generate the rules: {end - start} segs. ")
+
+# -------------------------------------
+rules_2_items = get_rules(movilens.columns, n_subitemset=2)
+rules_2_items_metrics_df = get_rules_metrics_df(rules_2_items, movilens)
+
+rules_3_items = get_rules(movilens.columns, n_subitemset=3)
+rules_3_items_metrics_df = get_rules_metrics_df(rules_3_items, movilens)
+
+# concat the two dataframes
+rules_metrics_df = pd.concat([rules_2_items_metrics_df, rules_3_items_metrics_df])
 rules_metrics_df = rules_metrics_df.sort_values(by=['support', 'confidence', 'lift'], ascending=[False, False, False])
+print(rules_metrics_df.head(20))
 
-print(f"\nTime elapsed to generate the rules: {end - start} segs. ")
+# El soporte mínimo de las reglas es 0.0
+print(f"\nMínimo soporte en el dataframe: {rules_metrics_df[(rules_metrics_df['confidence'].notna()) & (rules_metrics_df['lift'].notna())]['support'].min()}")
+
+frequent_itemsets = apriori(movilens, min_support=1e-5, use_colnames=True)
+print(frequent_itemsets)
+
+# El dataframe se compone de dos columnas, la primera muestra el support y la segunda los itemsets.
+# Se observa, como es evidente, que los itemsets con soporte más bajo son aquellos que contienen más elementos.
+# De manera contraria, los itemsets con soporte más alto son aquellos que contienen menos elementos.
+# Hay un total de 4076 itemsets con un soporte mínimo de 1e-5
+
+assoc_rules_df = association_rules(frequent_itemsets, num_itemsets=len(movilens))
+print(assoc_rules_df)
+# OJO: la versión de mlxtend 0.23.3 requiere el argumento num_itemsets para obtener todas las reglas posibles
+print(f"\nTotal de reglas generadas: {len(assoc_rules_df)}")
+
+# EL dataframe contiene, a parte de los consecuentes y antecedentes, una serie de métricas
+# que sirven parahacer un pruning posterior sobre las reglas a partir de un threshold establecido sobre 
+# la metrica deseada.
+
+# Se transforma el dataframe para que muestre "rule", "support", "confidence", "lift"
+assoc_rules_df['rule'] = assoc_rules_df.apply(lambda row: f"{list(row['antecedents'])} -> {list(row['consequents'])}", axis=1)
+assoc_rules_df = assoc_rules_df[['rule', 'support', 'confidence', 'lift']]
+assoc_rules_df = assoc_rules_df.sort_values(by=['support', 'confidence', 'lift'], ascending=[False, False, False])
+print(assoc_rules_df.head(20))
 
 print(rules_metrics_df.head(20))
 
-# Número de reglas de tipo A,B -> C ó A -> B,C para este dataset
-n_subitemset = 3
-print(f"\nNumber of rules of type A,B -> C or A -> B,C: {get_number_of_rules(n_itemset=len(movilens.columns), n_subitemset=n_subitemset)}")
+# Curiosamente, las 20 mejores reglas generadas en el punto 2 (reglas de 2 y 3 elementos), son reglas compuestas por 2 items, 1 de antecedente
+# Por otro lado, las 20 mejores reglas generadas a partir de la librería mlxtend, son reglas compuestas por más items. Donde el support máximo es
+# 0.003168, frente al support de la tabla anterior que es 0.075536. Esto se debe a que la librería mlxtend realiza un pruning de los items
+# a partir de `apriori` con la métrica support, de tal manera que itemsets donde la presencia de un item no supere el threshold, no se tienen en cuenta.
 
-start = time.time()
-rules = get_rules(movilens.columns, n_subitemset=3)
-end = time.time()
-rules_metrics_df = get_rules_metrics_df(rules, movilens)
-rules_metrics_df = rules_metrics_df.sort_values(by=['support', 'confidence', 'lift'], ascending=[False, False, False])
+## Intersección
+# interseccion entre riles_metrics_df y assoc_rules_df
+print(f"\nInterseccion entre rules_metrics_df y assoc_rules_df")
+rules_intersection = pd.merge(rules_metrics_df, assoc_rules_df, on='rule', how='right', suffixes=('_1', '_2'))
+print(rules_intersection)
+print(f"\nTotal de reglas en la intersección: {len(rules_intersection)}")
 
-print(f"\nTime elapsed to generate the rules: {end - start} segs. ")
-print(rules_metrics_df.head(20))
-
-# Número de reglas de 9 elementos para este dataset. Considero que cada antecendete puede tener un máximo de 8 elementos
-# y un mínimo de 1 elemento
-n_subitemset = 9
-print(f"\nNumber of rules for {n_subitemset} itemset: {get_number_of_rules(n_itemset=len(movilens.columns), n_subitemset=n_subitemset)}")
-
-start = time.time()
-rules = get_rules(movilens.columns, n_subitemset=9)
-end = time.time()
-print(f"\nTime elapsed to generate the rules: {end - start} segs. ")
-
-# Número de reglas de todo tipo que contengan desde 1 hasta 19 elementos (todas las reglas posibles)
-n_subitemset = len(movilens.columns)
-print(f"\nNumber of rules for {n_subitemset} itemset: {get_number_of_rules(n_itemset=len(movilens.columns), n_subitemset=n_subitemset)}")
-
-start = time.time()
-rules = get_rules(movilens.columns, n_subitemset=len(movilens.columns))
-end = time.time()
-print(f"\nTime elapsed to generate the rules: {end - start} segs. ")
+# La intersección entre los dos datarframes da lugar a 11 reglas. Es decir, de las reglas generadas a partir de mlxtend con un theshold de 1e-5
+# sobre los items y support con `apriori` y las reglas generadas a partir de la función `get_rules_metrics_df` sin tener en cuenta thresholds, 11 son comunes.
