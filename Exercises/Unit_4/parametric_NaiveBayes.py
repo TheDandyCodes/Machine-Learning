dededf66c63ec261f3a0200c33e103f1aec9cf1e
TheDandyCodes <ricardoprietoaiml@gmail.com>
2025-02-08 18:19:25 +0100

    feat: parametric Niave Bayes (Bernoulli, Gauss, Multinomial)

--- Exercises/Unit_4/parametric_NaiveBayes.py
+++ Exercises/Unit_4/parametric_NaiveBayes.py
@@ -2,7 +2,7 @@ from typing import Optional
 
 import numpy as np
 import pandas as pd
-from scipy.stats import norm, multinomial
+from scipy.stats import bernoulli, multinomial, norm
 from tqdm import tqdm
 
 
@@ -84,6 +84,7 @@ class NaiveBayes:
         laplace_smth_result = (result + alpha) / (
             result.sum(axis=1) + alpha * len(X_train[column].unique())
         ).values[:, None]
+
         laplace_smth_result_dict = laplace_smth_result.to_dict(orient="index")
         return laplace_smth_result_dict
 
@@ -115,12 +116,12 @@ class NaiveBayes:
                     feature
                 ].std()
 
-            for feature in X_train.select_dtypes(include="object").columns:
+            for feature in X_train.select_dtypes(include=["object", "bool"]).columns:
                 laplace_smth_result_dict = self._discret_likelihood_with_laplace_smth(
                     X_train, y_train, feature
                 )
                 parameters[class_][feature] = laplace_smth_result_dict[class_]
-        print(parameters)
+
         self.parameters = parameters
 
     def predict_prob(self, X_test: pd.DataFrame) -> dict[str, pd.Series]:
@@ -140,9 +141,27 @@ class NaiveBayes:
         for class_ in self.classes:
             likelihood = 1
             for feat in X_test.columns:
-                if X_test[feat].dtype == "object":
-                    likelihood *= X_test[feat].map(self.parameters[class_][feat])
-                else:
+                if X_test[feat].dtype == "bool":  # Binary variable (Bernoulli)
+                    # The Bernoulli PMF also uses a ratio (p) that is equivalent to
+                    # the relative frequency in the dataset.
+                    likelihood *= bernoulli.pmf(
+                        k=X_test[feat],
+                        p=self.parameters[class_][feat][True],
+                    )  # parameter p is the probability of the class being 'yes'.
+                    # If the class is 'no', the probability is 1 - p
+                elif X_test[feat].dtype == "object":
+                    # Categorical variable using Multinomial distribution
+                    likelihood *= multinomial.pmf(
+                        x=np.array(pd.get_dummies(X_test[feat])),
+                        # Since only one category per row is observed
+                        # (e.g., a single category of marital), n is set to 1.
+                        n=1,
+                        p=[
+                            self.parameters[class_][feat][col]
+                            for col in pd.get_dummies(X_test[feat]).columns
+                        ],  # Same order as the columns of the dummies
+                    )
+                else:  # Continuous variable using Gaussian distribution
                     likelihood *= norm.pdf(
                         X_test[feat],
                         self.parameters[class_][feat]["mean"],
@@ -169,23 +188,26 @@ class NaiveBayes:
         for class_ in self.classes:
             likelihood = 0
             for feat in X_test.columns:
-                if X_test[feat].dtype == "object":
-                    if len(X_test[feat].unique()) == 2:  # Binary variable (Bernoulli)
-                        # The Bernoulli PMF also uses a ratio (p) that is equivalent to
-                        # the relative frequency in the dataset.
-                        likelihood += np.log(
-                            X_test[feat].map(self.parameters[class_][feat])
-                        )
-                    else:  # Categorical variable (Multinomial)
-                        likelihood += np.log(
-                            X_test[feat].map(self.parameters[class_][feat])
-                        )
-                        # likelihood += multinomial.logpmf(
-                        #     X_test[feat].map(self.parameters[class_][feat]).values,
-                        #     n=1,
-                        #     p=1,
-                        # )
-                else:  # Continuous variable
+                if X_test[feat].dtypes == "bool":  # Binary variable (Bernoulli)
+                    # The Bernoulli PMF also uses a ratio (p) that is equivalent to
+                    # the relative frequency in the dataset.
+                    likelihood += bernoulli.logpmf(
+                        k=X_test[feat],
+                        p=self.parameters[class_][feat][True],
+                    )  # parameter p is the probability of the class being 'yes'.
+                    # If the class is 'no', the probability is 1 - p
+                elif X_test[feat].dtype == "object":  # Cat variable using Multinomial
+                    likelihood += multinomial.logpmf(
+                        x=np.array(pd.get_dummies(X_test[feat])),
+                        # Since only one category per row is observed
+                        # (e.g., a single category of marital), n is set to 1.
+                        n=1,
+                        p=[
+                            self.parameters[class_][feat][col]
+                            for col in pd.get_dummies(X_test[feat]).columns
+                        ],  # Same order as the columns of the dummies
+                    )
+                else:  # Continuous variable using Gaussian distribution
                     likelihood += norm.logpdf(
                         X_test[feat],
                         self.parameters[class_][feat]["mean"],
@@ -212,7 +234,7 @@ class NaiveBayes:
         pd.Series
             Predicted class.
         """
-        if method == "log":
+        if method == "log":  # noqa: SIM108
             probabilities = pd.DataFrame(self.predict_log_prob(X_test))
         else:
             probabilities = pd.DataFrame(self.predict_prob(X_test))
@@ -304,13 +326,52 @@ if __name__ == "__main__":
     print("Loading datasets...\n\n")
     from ucimlrepo import fetch_ucirepo
 
+    def two_categories_to_binary(df: pd.DataFrame) -> pd.DataFrame:
+        """Encode two categories variables into binary variables.
+
+        Parameters
+        ----------
+        df : pd.DataFrame
+            Dataframe with some columns with two categories
+
+        Returns
+        -------
+        pd.DataFrame
+            Dataframe with encoded variables
+        """
+        # Select columns with only two categories
+        binary_columns = (
+            df.select_dtypes(include="object")
+            .nunique()[df.select_dtypes(include="object").nunique() == 2]
+            .index
+        )
+        for col in binary_columns:
+            df[col] = pd.get_dummies(df[col], drop_first=True)
+        return df
+
     iris_dataset = fetch_ucirepo(id=53)
     X_iris = iris_dataset.data.features
     y_iris = iris_dataset.data.targets["class"]
     iris_df = pd.concat([X_iris, y_iris], axis=1)
 
     bank_marketing = fetch_ucirepo(id=222)
-    X_bank_marketing = bank_marketing.data.features
+    # Secure independent copy to avoid view problems
+    X_bank_marketing = bank_marketing.data.features.copy()
+    # Replace 'yes' and 'no' with binary values
+    for col in ["default", "housing", "loan"]:
+        X_bank_marketing[col] = X_bank_marketing[col].map({"yes": True, "no": False})
+        # Para las variables con 2 categorías (no binarias), se introducirán en la distribución Multinomial,
+        # ya que en casos como la variable `stalk-shape`, sus valores `enlarging=e`,`tapering=t` no son conceptualmente opuestos,
+        # como una varibale binaria. Es decir, si asumieramos que elarging es True y tapering es False,
+        # estaríamos concluyendo que concepturalmente elarging es lo opuesto a tapering.
+
+        # De esta manera, si codificaramos la variable `stalk-shape` como binaria, tendriamos que escoger
+        # qué categoría considerar como True y cual como False, haciendo que realmente la categoría que cuenta,
+        # conceptualmente, sea a la que le asociamos el valor de True, por ejemplo elarging. De esta manera la
+        # variable sería menos interpretable. Es decir, sería necesario que esa variable se llamara Elarging y
+        # que sus valores fueran True o False, es decir, está ensanchando (True) o no está ensanchando (False).
+        # Por eso mismo, solo se utilizará la distribución Bernouilli para las variables conceptualmente binarias,
+        # como puede ser `default`, donde True equivaldría a que se ha dado default y False que no.
     y_bank_marketing = bank_marketing.data.targets["y"]
     bank_marketing_df = pd.concat([X_bank_marketing, y_bank_marketing], axis=1)
     bank_marketing_df_no_misssing = bank_marketing_df.dropna(
@@ -319,8 +380,16 @@ if __name__ == "__main__":
     bank_marketing_df_no_misssing = bank_marketing_df_no_misssing.drop(
         columns=["contact", "poutcome"]
     )
-    X_bank_marketing_df_no_misssing = bank_marketing_df_no_misssing.iloc[:, :-1]
-    y_bank_marketing_df_no_misssing = bank_marketing_df_no_misssing["y"]
+
+    mushrooms = fetch_ucirepo(id=73)
+    X_mushrooms = mushrooms.data.features.copy()
+    X_mushrooms = X_mushrooms.drop(
+        columns="stalk-root"
+    )  # Drop column w/ missing values
+    # Replace 'f' and 't' with binary values
+    X_mushrooms["bruises"] = X_mushrooms["bruises"].map({"f": False, "t": True})
+    y_mushrooms = mushrooms.data.targets  # p = poisonous, e = edible
+    mushrooms_df = pd.concat([X_mushrooms, y_mushrooms], axis=1)
 
     print("## IRIS DATASET ##\n")
     print("Fitting the models...\n")
@@ -356,15 +425,41 @@ if __name__ == "__main__":
         print(f"Fold {i} - Accuracy: {fold_acc}")
     print(f"Mean Accuracy: {cv_ev_log[1]}\n\n")
 
+    print("## MUSHROOMS DATASET ##\n")
+    print("Fitting the models...\n")
+    nb = NaiveBayes()
+
+    print("Evaluating the model with NO log-probabilities...\n")
+    cv_ev = nb.cross_validation_evaluate(k=5, data=mushrooms_df)
+    for i, fold_acc in enumerate(cv_ev[0]):
+        print(f"Fold {i} - Accuracy: {fold_acc}")
+    print(f"Mean Accuracy: {cv_ev[1]}\n\n")
+
+    print("Evaluating the model with log-probabilities...\n")
+    cv_ev_log = nb.cross_validation_evaluate(k=5, data=mushrooms_df, method="log")
+    for i, fold_acc in enumerate(cv_ev_log[0]):
+        print(f"Fold {i} - Accuracy: {fold_acc}")
+    print(f"Mean Accuracy: {cv_ev_log[1]}\n\n")
+
+    # En esta modificaciíon del código se emplean las distribuciones
+    # Multinomial, Bernoulli y Normal para calcular las probabilidades.
+    # Esto no cambia el valor de las probabilidades de una feature por clase.
+    # Por un lado, la distribución Bernoulli usa el ratio (p), que es equivalente a
+    # la frecuencia relativa en el dataset, es decir, para una variable binaria,
+    # la frecuencia relativa del valor de la variable para cada clase es equivalente
+    # a la probabilidad de la variable para cada clase según la distribución.
+    # Por otro lado, la distribución Multinomial se emplea para variables categóricas
+    #
+
     # ## Resultados de la evalaución del modelo con el dataset IRIS ##
     # Evaluating the model with NO log-probabilities...
 
     # Fold 0 - Accuracy: 1.0
-    # Fold 1 - Accuracy: 0.9666666666666667
-    # Fold 2 - Accuracy: 0.9666666666666667
-    # Fold 3 - Accuracy: 0.9333333333333333
-    # Fold 4 - Accuracy: 0.9666666666666667
-    # Mean Accuracy: 0.9666666666666668
+    # Fold 1 - Accuracy: 0.9
+    # Fold 2 - Accuracy: 0.9333333333333333
+    # Fold 3 - Accuracy: 1.0
+    # Fold 4 - Accuracy: 0.9333333333333333
+    # Mean Accuracy: 0.9533333333333334
 
     # Estos resultados indican que el modelo tiene un buen desempeño en términos
     # de Accuracy (Predicciones correctas / Total de predicciones)
@@ -375,12 +470,12 @@ if __name__ == "__main__":
 
     # Evaluating the model with log-probabilities...
 
-    # Fold 0 - Accuracy: 0.9666666666666667
+    # Fold 0 - Accuracy: 1.0
     # Fold 1 - Accuracy: 0.9666666666666667
-    # Fold 2 - Accuracy: 0.8333333333333334
-    # Fold 3 - Accuracy: 1.0
-    # Fold 4 - Accuracy: 1.0
-    # Mean Accuracy: 0.9533333333333334
+    # Fold 2 - Accuracy: 0.9
+    # Fold 3 - Accuracy: 0.9
+    # Fold 4 - Accuracy: 0.9666666666666667
+    # Mean Accuracy: 0.9466666666666667
 
     # En este caso, se emplea el logaritmo de las probabilidades para evitar problemas
     # de underflow, dado que las probabilidades son valores muy pequeños pudiendo ser
@@ -389,12 +484,12 @@ if __name__ == "__main__":
     # ## Resultados de la evalaución del modelo con el dataset BANK MARKETING ##
     # Evaluating the model with NO log-probabilities...
 
-    # Fold 0 - Accuracy: 0.8901493228382915
-    # Fold 1 - Accuracy: 0.8752170390091446
-    # Fold 2 - Accuracy: 0.8878342400740826
-    # Fold 3 - Accuracy: 0.8797175272053717
-    # Fold 4 - Accuracy: 0.878212549201204
-    # Mean Accuracy: 0.8822261356656188
+    # Fold 0 - Accuracy: 0.8837828452367172
+    # Fold 1 - Accuracy: 0.8745225141798819
+    # Fold 2 - Accuracy: 0.8732492186595671
+    # Fold 3 - Accuracy: 0.8765918036582542
+    # Fold 4 - Accuracy: 0.8831905533688353
+    # Mean Accuracy: 0.878267387020651
 
     # En este caso, el modelo tiene un desempeño aceptable en términos de Accuracy
     # en la clasificación de los clientes (Sí se suscribe a un depósito, No se suscribe
@@ -404,12 +499,31 @@ if __name__ == "__main__":
 
     # Evaluating the model with log-probabilities...
 
-    # Fold 0 - Accuracy: 0.8845931242041903
-    # Fold 1 - Accuracy: 0.8789211714318786
-    # Fold 2 - Accuracy: 0.8845931242041903
-    # Fold 3 - Accuracy: 0.8790229219726788
-    # Fold 4 - Accuracy: 0.8851586015281315
-    # Mean Accuracy: 0.8824577886682141
+    # Fold 0 - Accuracy: 0.8747540224563028
+    # Fold 1 - Accuracy: 0.8764903345294595
+    # Fold 2 - Accuracy: 0.8818150248871397
+    # Fold 3 - Accuracy: 0.8768233387358185
+    # Fold 4 - Accuracy: 0.880064829821718
+    # Mean Accuracy: 0.8779895100860877
 
     # En este caso, se emplea el logaritmo de las probabilidades para evitar problemas
     # de underflow. El desempeño es similar al modelo sin logaritmos.
+
+    # ## Resultados de la evalaución del modelo con el dataset MUSHROOMS ##
+    # Evaluating the model with NO log-probabilities...
+
+    # Fold 0 - Accuracy: 0.9501538461538461
+    # Fold 1 - Accuracy: 0.9661538461538461
+    # Fold 2 - Accuracy: 0.963076923076923
+    # Fold 3 - Accuracy: 0.96
+    # Fold 4 - Accuracy: 0.9636699507389163
+    # Mean Accuracy: 0.9606109132247063
+
+    # Evaluating the model with log-probabilities...
+
+    # Fold 0 - Accuracy: 0.9581538461538461
+    # Fold 1 - Accuracy: 0.9643076923076923
+    # Fold 2 - Accuracy: 0.9655384615384616
+    # Fold 3 - Accuracy: 0.9575384615384616
+    # Fold 4 - Accuracy: 0.9593596059113301
+    # Mean Accuracy: 0.9609796134899582
